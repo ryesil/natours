288a1c5fb02f48aceddec72e6e058e1f005e08e7
ryesil <ramazanysl@gmail.com>
2023-01-22 21:54:31 -0600

    Database connection; implementing schema and model for mongoose

--- public/eben.js
+++ public/eben.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-extend-native */
 Date.prototype.getDayOfTheWeek = (day)=>{
     const days = ['Sunday','Monday',"Tuesday",'Wednesday','Thursday','Friday','Saturday']
     return days[day]
 }
 
 const cDay = new Date();
-const isAm = cDay.getHours()*1<12 ?true:false;
+const isAm = cDay.getHours()*1<12;
 
-console.log(cDay.getHours()+(isAm? " AM ":" PM ")+" : "+cDay.getMinutes()+ " : "+cDay.getSeconds())
+console.log(`${cDay.getHours()+(isAm? " AM ":" PM ")} : ${cDay.getMinutes() } : ${cDay.getSeconds()}`)
 
-console.log("Today is "+cDay.getDayOfTheWeek(cDay.getDay()))
+console.log(`Today is ${cDay.getDayOfTheWeek(cDay.getDay())}`)
 const secondTime =new Date().toISOString()
 const t = secondTime.indexOf('T')
 // console.log(secondTime.substring(0,t))
@@ -22,29 +25,32 @@ const dayFormat = function(){
 let d= date.getDay();
 // console.log(d)
 let m = date.getMonth()+1;
-let y = date.getFullYear();
+const y = date.getFullYear();
     if (d<10)
-    d= "0"+d;
+    d= `0${d}`;
     if (m<10)
-    m="0"+m;
-    return d+"/"+m+'/'+y
+    m=`0${m}`;
+    return `${d}/${m}/${y}`
 }
 // console.log(dayFormat())
 
 const areaOfTriangle = function(){
-    u = (5+6+7)*1.0/2;
+   const u = (5+6+7)*1.0/2;
 
     return Math.round(Math.sqrt(u*(u-5)*(u-6)*(u-7))) 
 }
 
 //console.log(areaOfTriangle())
 
+//url of the page http://localhost:3000/exercise.html
+console.log(document.location.href)
+
+const getExtention = function(fileName){
+    return fileName.split('.').pop();
 
 
 
+}
 
-
-
-
-
+console.log(getExtention("sadasdasd.java"))
 

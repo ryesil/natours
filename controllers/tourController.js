9eba4bdc01c115f0f73cd3ee7693340f2213fde8
Ramazan Yesil <85940052+ryesil@users.noreply.github.com>
2023-01-23 21:27:44 -0600

    Ry 11 (#15)
    * configured npm eslint prettier distinguished dev,  prod, and environment variables
    
    * Database connection; implementing schema and model for mongoose
    
    * used mongoose to make the new tour. Implemented models and save() to DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,140 +1,51 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
-const fs = require('fs');
-//we read the data before we send it. we will read the data so we dont need async but sync
-//__dirname is the where the current script app.js is located which is the
-// main folder (/Downloads/complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter ).
-//Since the file is String we need to parse it to Json object
-//Below __dirname is not working b/c directory is now routes folder.
-// const tours =JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    //below is json formatting standard
-    status: 'success', //success,fail, or error
-    data: {
-      tours: tours,
-    },
+    status: 'success',
   });
 };
 
-//Getting one tour
-//We used it in route.param middleware. Instead of checking for id in every req. This will deter it from happining
-exports.isIdExist = (req, res, next, val) => {
-  // eslint-disable-next-line no-shadow
-  const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-  );
-  const idList = [];
-  tours.forEach((el) => {
-    idList.push(el.id);
-  });
-  if (!idList.includes(val * 1)) {
-    //the return is important here. If we don't return then express will continue running and
-    //it will try to send another response which will cause an error.
-    //The idea here is after we return next function will not be called so no more middlewares after this onee.
-    return res.status(404).json({
-      status: 'fail',
-      message: `No tour exists with id ${req.params.id}`,
-    });
-  }
-  next();
-};
 
 //: means variable
 exports.getTour = (req, res) => {
-  // app.get('/api/v1/tours/:id/:x/:v/:f/:r/:e?',(req,res)=>{
-  //Question mark at the end makes it optional not required
-  //Here is the log => { id: ':4', x: ':5', v: ':6', f: ':7', r: ':8', e: ':9' }
-  //keys are the ones we define here in the path. Values come from the get request.
-  console.log(req.params);
-  const id = req.params.id * 1;
-  //If there is no tour it will be undefined
-  const tour = tours.find((el) => el.id === id);
 
-  //if(id>toursLength-1){
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: `No such tour exists with ${id}`,
-    });
-  } else {
-    res.status(200).json({
-      status: 'success',
-      data: {
-        //tour:tour
-        tour,
-      },
-    });
+};
+
+// We used try catch b/c of the async/await (promise return)
+exports.makeTour = async (req, res) => {
+ //req.body
+ //we did using model like let newTour = new Tour({...})
+//newTour.save(); Below is the new Way. Returns a promise
+//Since Tour is an async function we can write async at the begining of the function insted of writing then() below
+try {
+const newTour = await Tour.create(req.body)
+
+res.status(201).json({
+  status:'success',
+  data:{
+    tour:newTour
   }
+})//validation error will be taken 4oo is for bad request
+} catch(err) {
+res.status(400).json({
+  status:'fail',
+  //For now
+  message:"Invalid data sent!"
+})
+
+}
+
 };
 
-exports.makeTour = (req, res) => {
-  //body is available on the request
-  //console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-  //now we need to persist data to the file
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours, null, '\t'),
-    () => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          newTour: newTour,
-        },
-      });
-    }
-  );
-};
+
 
 exports.updateTour = (req, res) => {
-  //get the file change and then save it again
-  const tour = tours.find((el) => el.id === req.params.id * 1);
-  for (let i = 0; i < Object.keys(req.body).length; i++) {
-    tour[Object.keys(req.body)[i]] = Object.values(req.body)[i];
-  }
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours, null, '\t'),
-    () => {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
-    }
-  );
-  //console.log(req.body)
+ 
 };
 
 exports.deleteTour = (req, res) => {
-  const newTours = tours.filter((el) => el.id !== req.params.id * 1);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(newTours, null, '\t'),
-    () => {
-      //202 => no Content
-      res.status(202).json({
-        status: 'success',
-        data: null,
-      });
-    }
-  );
+
 };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'The body must contain both name and price',
-    });
-  }
-  next();
-};
